76c3cc1fb79dcbfd43e7ee17ce913d1631580bb1
Sergei <40200071+Sergei1790@users.noreply.github.com>
2024-09-03 10:16:15 +0300

    Done

---
+++ src/js/index.test.js
@@ -0,0 +1,7 @@
+import {describe, expect, test} from '@jest/globals';
+import { sum } from './index';
+describe('Calculator', () => {
+    test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+    });
+});
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const ESLintWebpackPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  // entry: './src/index.js',
+  mode: 'development',
+  entry: {
+    index: './src/js/index.js',
+    // print: './src/js/print.js',
+  },
+  output: {
+    // filename: 'bundle.js',
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  // devServer: {
+  //   static: './dist',
+  // },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+      minify: false,
+    }),
+    // new ESLintWebpackPlugin({ // Add the plugin here
+    //   extensions: ['js', 'jsx'], // Specify the file extensions to lint
+    //   exclude: ['node_modules'], // Exclude certain directories
+    //   fix: true, // Automatically fix linting errors where possible
+    // }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
+
